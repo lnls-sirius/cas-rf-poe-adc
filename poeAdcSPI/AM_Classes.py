90619fd568d8be7e588b50224266ed6bc7e62bb3
carneirofc <claudiofcarneiro@hotmail.com>
2019-08-01 12:15:21 -0300

    SPI: Trimming

--- poeAdcSPI/AM_Classes.py
+++ poeAdcSPI/AM_Classes.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from Adafruit_BBIO.SPI import SPI
 import Adafruit_BBIO.GPIO as GPIO
-import time
-
+from Adafruit_BBIO.SPI import SPI
 
 class ADC:
     
@@ -20,11 +18,6 @@ class ADC:
         self.spi.xfer2([0xff,0xff])
         GPIO.output(self.port,1)
         
-        #Configuração do ADC
-        GPIO.output(self.port,0)
-        self.spi.xfer2([0xff,0xff])
-        GPIO.output(self.port,1)
-    
         #Envio de setup inicial
         GPIO.output(self.port,0)
         self.spi.xfer2([0x83,0x10])
@@ -32,13 +25,11 @@ class ADC:
     
 
     def __str__(self):
-        return "Este ADC está localizado na porta: " + self.port
+        return "ADC localizado porta: " + self.port
         
         
     def read(self,channels):
-        "Retorna valor medido pelo ADC (Valor de 0 à 4095, necessario fazer a conversão de acordo com os resistores R e R*)"
-        "channels é uma lista de dados, ou um canal apenas."
-        
+        " Retorna valor medido pelo ADC (Valor de 0 4095) "
         saida = []
         if type(channels) == list:
             tamanho = len(channels)
@@ -46,38 +37,27 @@ class ADC:
             _channels = channels[:]
             _channels.append(channels[tamanho-1]+1)
             for i in _channels:
-                GPIO.output(self.port,0)
-                #data = self.spi.xfer2([0x83+(2**2)*i,0x10])
-                data = self.spi.xfer2([0x83+(4)*i,0x10])
-                GPIO.output(self.port,1)
-                #calc = (data[0] - (i-1)*(2**4)) * (2**8) + data[1]
+                GPIO.output(self.port, 0)
+                data = self.spi.xfer2([0x83 + (4)*i, 0x10])
+                GPIO.output(self.port, 1)
                 calc = (data[0] - (i-1)*(16)) * (256) + data[1]
-                #if calc > (2**12):
                 if calc > (4096):
                     calc = 0
                 saida.append(calc)
-                #time.sleep(0.01)
-
             return saida[1:]
-            
-            
         else:
-            
             c = channels
             for i in range(2):
                 GPIO.output(self.port,0)
                 data = self.spi.xfer2([0x83+(4)*c,0x10])
-                #data = self.spi.xfer2([0x83+(2**2)*c,0x10])
                 GPIO.output(self.port,1)
-            #calc = (data[0] - (c)*(2**4)) * (2**8) + data[1]
             calc = (data[0] - (c)*(16)) * (256) + data[1]
-            #if calc > 2**12:
             if calc > 4096:
                 calc = 0
             saida = (calc)
             return saida
             
-        
+''' 
         
 def convert(ADC,Vref,R,R_):
     "ADC é o valor a ser convertido, R e R_ são os resistores que variam na PCB,VCC é a tensão máxima"
@@ -104,4 +84,4 @@ def value(channels, R1= [], R2= [], R3= [], R4= [], R5= [], R6= [], R7= [], R8 =
 	if type(channels) == list:
 		if (len(lista)/2) != len(channels):
 			print("Please insert the Resistors values [R, R_]. R* = R & R** = R_")
-        
+'''        
