16aedfa20575882cb105349ef4fce41a1b6d713d
carneirofc <claudiofcarneiro@hotmail.com>
2019-08-30 12:26:37 -0300

    db: Database generation and PV name

--- poeAdcApp/Db/gen.py
+++ poeAdcApp/Db/gen.py
@@ -2,36 +2,12 @@
 from string import Template
 voltage = Template('''
 record(calc, "${NAME}"){
-    field(CALC, "(${CH}*A/4095.)*(${CTE})")
+    field(CALC, "(5.*A/4095.)*(${CTE})")
     field(INPA, "$(P)$(R):ADC${ADC}:Data-Mon.VAL[${CH}] CP MSS")
     field(EGU, "V")
     field(DESC, "${DESC}")
 }
 ''')
-
-#Descrição Lógica Nome EP
-#Monitoramento da Tensão de Saída do Detector de Potência 1RA-RaSIA01:LLRFCalSys:PwrdBm1-Mon
-#Monitoramento da Tensão de Saída do Detector de Potência 2RA-RaSIA01:LLRFCalSys:PwrdBm2-Mon
-#Monitoramento da Tensão de Saída do Detector de Potência 3RA-RaSIA01:LLRFCalSys:PwrdBm3-Mon
-#Monitoramento da Tensão de Saída do Detector de Potência 4RA-RaSIA01:LLRFCalSys:PwrdBm4-Mon
-        
-#Descrição Lógica Nome EP
-#Monitoramento da Tensão de Saída do Detector de Potência 5RA-RaSIA01:LLRFCalSys:PwrdBm5-Mon
-#Monitoramento da Tensão de Saída do Detector de Potência  RA-RaSIA01:LLRFCalSys:PwrdBm6-Mon
-#Monitoramento da Tensão de Saída do Detector de Potência  RA-RaSIA01:LLRFCalSys:PwrdBm7-Mon
-#Monitoramento da Tensão de Saída do Detector de Potência  RA-RaSIA01:LLRFCalSys:PwrdBm8-Mon
-                                
-#Descrição Lógica Nome EP
-#Monitoramento da Tensão de Saída do Detector de Potência 9RA-RaSIA01:LLRFCalSys:PwrdBm9-Mon
-#Monitoramento da Tensão de Saída do Detector de Potência 1RA-RaSIA01:LLRFCalSys:PwrdBm10-Mon
-#Monitoramento da Tensão de Saída do Detector de Potência 1RA-RaSIA01:LLRFCalSys:PwrdBm11-Mon
-#Monitoramento da Tensão de Saída do Detector de Potência 1RA-RaSIA01:LLRFCalSys:PwrdBm12-Mon
-                                                                        
-#Descrição Lógica Nome ES
-#Monitoramento da Tensão de Saída do Detector de Potência 1RA-RaSIA01:LLRFCalSys:PwrdBm13-Mon
-#Monitoramento da Tensão de Saída do Detector de Potência 1RA-RaSIA01:LLRFCalSys:PwrdBm14-Mon
-#Monitoramento da Tensão de Saída do Detector de Potência 1RA-RaSIA01:LLRFCalSys:PwrdBm15-Mon
-#Monitoramento da Tensão de Saída do Detector de Potência 1RA-RaSIA01:LLRFCalSys:PwrdBm16-Mon
 
 def res(R, R_):
     return str(((R_+R)/R_))
@@ -39,49 +15,43 @@ def res(R, R_):
 defaults = {'CTE':'1', 'DESC':''}
 
 dbs = [
-    {'ioc':'ioc1.db',
+    {'ioc':'SIA-CalSys.db',
     'readings' : [
-        {'ADC':'0', 'CH':'0', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm1-Mon', 'template':voltage},
-        {'ADC':'0', 'CH':'1', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm2-Mon', 'template':voltage},
-        {'ADC':'0', 'CH':'2', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm3-Mon', 'template':voltage},
-        {'ADC':'0', 'CH':'3', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm4-Mon', 'template':voltage},
+        {'ADC':'0', 'CH':'0', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm1-Mon', 'template':voltage},
+        {'ADC':'0', 'CH':'1', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm2-Mon', 'template':voltage},
+        {'ADC':'0', 'CH':'2', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm3-Mon', 'template':voltage},
+        {'ADC':'0', 'CH':'3', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm4-Mon', 'template':voltage},
 
-        {'ADC':'1', 'CH':'0', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm5-Mon', 'template':voltage},
-        {'ADC':'1', 'CH':'1', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm6-Mon', 'template':voltage},
-        {'ADC':'1', 'CH':'2', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm7-Mon', 'template':voltage},
-        {'ADC':'1', 'CH':'3', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm8-Mon', 'template':voltage},
+        {'ADC':'1', 'CH':'0', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm5-Mon', 'template':voltage},
+        {'ADC':'1', 'CH':'1', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm6-Mon', 'template':voltage},
+        {'ADC':'1', 'CH':'2', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm7-Mon', 'template':voltage},
+        {'ADC':'1', 'CH':'3', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm8-Mon', 'template':voltage},
 
-        {'ADC':'2', 'CH':'0', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm9-Mon ', 'template':voltage},
-        {'ADC':'2', 'CH':'1', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm10-Mon', 'template':voltage},
-        {'ADC':'2', 'CH':'2', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm11-Mon', 'template':voltage},
-        {'ADC':'2', 'CH':'3', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm12-Mon', 'template':voltage},
+        {'ADC':'2', 'CH':'0', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm9-Mon', 'template':voltage},
+        {'ADC':'2', 'CH':'1', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm10-Mon', 'template':voltage},
+        {'ADC':'2', 'CH':'2', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm11-Mon', 'template':voltage},
+        {'ADC':'2', 'CH':'3', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm12-Mon', 'template':voltage},
 
-        {'ADC':'3', 'CH':'0', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm13-Mon', 'template':voltage},
-        {'ADC':'3', 'CH':'1', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm14-Mon', 'template':voltage},
-        {'ADC':'3', 'CH':'2', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm15-Mon', 'template':voltage},
-        {'ADC':'3', 'CH':'3', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm16-Mon', 'template':voltage},
+        {'ADC':'3', 'CH':'0', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm13-Mon', 'template':voltage},
+        {'ADC':'3', 'CH':'1', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm14-Mon', 'template':voltage},
+        {'ADC':'3', 'CH':'2', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm15-Mon', 'template':voltage},
+        {'ADC':'3', 'CH':'3', 'NAME':'RA-RaSIA01:RF-LLRFCalSys:PwrdBm16-Mon', 'template':voltage},
     ]},
-    {'ioc':'ioc2.db',
-    'readings' : [
-        {'ADC':'0', 'CH':'0', 'NAME':'$(P)$(R):ADC1:CH1:Voltage-Mon', 'template':voltage},
-        {'ADC':'0', 'CH':'1', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm2-Mon', 'template':voltage},
-        {'ADC':'0', 'CH':'2', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm3-Mon', 'template':voltage},
-        {'ADC':'0', 'CH':'3', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm4-Mon', 'template':voltage},
+    {'ioc':'SIA-CavPlDrv.db',
+        'readings' : [
+        {'ADC':'0', 'CH':'0', 'NAME':'RA-RaSIA01:RF-CavPlDrivers:VoltPos5V-Mon',  'template':voltage},
+        {'ADC':'0', 'CH':'1', 'NAME':'RA-RaSIA01:RF-CavPlDrivers:Current5V-Mon',  'template':voltage},
+        {'ADC':'0', 'CH':'2', 'NAME':'RA-RaSIA01:RF-CavPlDrivers:VoltPos48V-Mon', 'template':voltage, 'CTE':res(90.9,10.)},
 
-        {'ADC':'1', 'CH':'0', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm5-Mon', 'template':voltage},
-        {'ADC':'1', 'CH':'1', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm6-Mon', 'template':voltage},
-        {'ADC':'1', 'CH':'2', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm7-Mon', 'template':voltage},
-        {'ADC':'1', 'CH':'3', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm8-Mon', 'template':voltage},
+        {'ADC':'1', 'CH':'0', 'NAME':'RA-RaSIA01:RF-CavPlDrivers:Dr1Current-Mon', 'template':voltage},
+        {'ADC':'1', 'CH':'1', 'NAME':'RA-RaSIA01:RF-CavPlDrivers:Dr1Enbl-Sts',    'template':voltage},
+        {'ADC':'1', 'CH':'2', 'NAME':'RA-RaSIA01:RF-CavPlDrivers:Dr1Flt-Mon',     'template':voltage},
+        {'ADC':'1', 'CH':'3', 'NAME':'SI-03SP:RF-P5Cav:Pl1Pos-Mon',               'template':voltage},
 
-        {'ADC':'2', 'CH':'0', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm9-Mon ', 'template':voltage},
-        {'ADC':'2', 'CH':'1', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm10-Mon', 'template':voltage},
-        {'ADC':'2', 'CH':'2', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm11-Mon', 'template':voltage},
-        {'ADC':'2', 'CH':'3', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm12-Mon', 'template':voltage},
-
-        {'ADC':'3', 'CH':'0', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm13-Mon', 'template':voltage},
-        {'ADC':'3', 'CH':'1', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm14-Mon', 'template':voltage},
-        {'ADC':'3', 'CH':'2', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm15-Mon', 'template':voltage},
-        {'ADC':'3', 'CH':'3', 'NAME':'RA-RaSIA01:LLRFCalSys:PwrdBm16-Mon', 'template':voltage},
+        {'ADC':'2', 'CH':'0', 'NAME':'RA-RaSIA01:RF-CavPlDrivers:Dr2Current-Mon', 'template':voltage},
+        {'ADC':'2', 'CH':'1', 'NAME':'RA-RaSIA01:RF-CavPlDrivers:Dr2Enbl-Sts',    'template':voltage},
+        {'ADC':'2', 'CH':'2', 'NAME':'RA-RaSIA01:RF-CavPlDrivers:Dr2Flt-Mon',     'template':voltage},
+        {'ADC':'2', 'CH':'3', 'NAME':'SI-03SP:RF-P5Cav:Pl2Pos-Mon',               'template':voltage},
     ]},
 ]
 
