1d7a618e094ab1edd43dc05c9f041277fc9f670e
carneirofc <claudiofcarneiro@hotmail.com>
2021-06-08 10:14:15 -0300

    iocBoot: Include LLRFSwPS-Spare cmd

---
+++ iocBoot/iocpoeAdc/LLRFSwPS-Spare.cmd
@@ -0,0 +1,23 @@
+#!../../bin/linux-arm/poeAdc
+
+## You may have to change poeAdc to something else
+## everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+epicsEnvSet("P", "SPARE")
+epicsEnvSet("D", "RF-LLRFSwPS")
+
+dbLoadDatabase "dbd/poeAdc.dbd"
+poeAdc_registerRecordDeviceDriver pdbbase
+
+dbLoadRecords("db/LLRFSwPS.db", "PORT=L0,A=0,P=$(P),D=$(D),S=.1")
+
+drvAsynIPPortConfigure("L0", "unix://$(TOP)/poeAdcSPI/unix-socket")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+#var streamDebug 1
