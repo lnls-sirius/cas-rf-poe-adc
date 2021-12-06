4d014d6c21d07f9928479eea2557a2405a5a6e92
carneirofc <claudiofcarneiro@hotmail.com>
2021-12-06 16:43:11 -0300

    Include BO-CalSys gen script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,3 +7,10 @@ pushd ../CalSysSup
 cat SIA-CalSys.db | grep record | grep PwrdBm | grep Mon | grep -Po '(?<=")(.*?)(?="\){)' | sed 's/.*/&\.DESC/' >SIA-CalSys.req
 cat SIA-CalSys.db | grep record | grep OFSdB | grep -Po '(?<=")(.*?)(?="\){)' >>SIA-CalSys.req
 popd
+
+# Booster Cal Sys (legacy scripts)
+./gen-cal-sys-legacy.py > ../CalSys/Sup/BO-CalSys.db
+pushd ../CalSysSup
+cat BO-CalSys.db | grep record | grep PwrdBm | grep Mon | grep -Po  '(?<=")(.*?)(?="\){)' | sed 's/.*/&\.DESC/' > BO-CalSys.req
+cat BO-CalSys.db | grep record | grep OFSdB | grep -Po  '(?<=")(.*?)(?="\){)' >> BO-CalSys.req
+popd
